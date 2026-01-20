b6729d24b7c66f4c73acc4c49105b93ae6aceda7
Abdo Tonio <abdlkbir.dacosta12@gmail.com>
2026-01-20 07:12:43 +0100

    adding the actuator and prometheus

--- gateway-service/src/main/java/org/example/gatewayservice/security/SecurityConfig.java
+++ gateway-service/src/main/java/org/example/gatewayservice/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // <--- 2. CORS Reactive
                 .authorizeExchange(exchanges -> exchanges // <--- 3. authorizeExchange (pas HttpRequests)
-                        .pathMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .pathMatchers("/v3/api-docs/**", "/swagger-ui/**", "/actuator/**").permitAll()
                         .pathMatchers("/api/products/**").authenticated() // Exemple
                         .anyExchange().authenticated()
                 )
